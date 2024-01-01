047c0ea21cbef09f70c2b8e8febe7b2788f3c77d
vg <jpatton@vgeek.net>
2023-12-31 20:25:21 -0500

    mlb sleep

--- sportsipy/mlb/roster.py
+++ sportsipy/mlb/roster.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 import re
 from functools import wraps
@@ -247,6 +249,7 @@ class Player(AbstractPlayer):
         """
         url = self._build_url()
         try:
+            time.sleep(8)
             url_data = pq(url)
         except HTTPError:
             return None
